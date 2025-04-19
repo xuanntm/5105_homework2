e4eee91fecf777d213ccb181592498ecc1f04147
Xuan Nguyen <xuan.ntm92@gmail.com>
2025-04-19 17:34:32 +0800

    update code using statsmodels library

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.10
+FROM python:3.12
 WORKDIR /app
 COPY . /app
 RUN pip install --no-cache-dir -r requirements.txt
--- app.py
+++ app.py
@@ -1,24 +1,38 @@
 from flask import Flask, request, jsonify
-from sklearn.linear_model import LinearRegression
-import numpy as np
+import pandas as pd
+import statsmodels.api as sm
+
 
 app = Flask(__name__)
 
-# Training data
-X = np.array([[0], [1], [2], [3], [4]])
-y = np.array([0, 2, 4, 6, 8])
-model = LinearRegression().fit(X, y)
+# training data
+data = {
+    'Y': [137, 118, 124, 124, 120, 129, 122, 142, 128, 114, 132, 130, 130, 112, 132, 117, 134, 132, 121, 128],
+    'W': [0, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1],
+    'X': [19.8, 23.4, 27.7, 24.6, 21.5, 25.1, 22.4, 29.3, 20.8, 20.2, 27.3, 24.5, 22.9, 18.4, 24.2, 21, 25.9, 23.2, 21.6, 22.8]
+}
+
+df = pd.DataFrame(data)
+# Prepare the independent and dependent variables
+X = sm.add_constant(df[['W', 'X']])  # Add constant for intercept
+Y = df['Y']
+
+# Fit the OLS model
+model = sm.OLS(Y, X).fit()
+print(model.summary())
 
 @app.route("/predict")
 def predict():
+    w = int(request.args.get("w", 0))
     x = float(request.args.get("x", 0))
-    y_pred = model.predict([[x]])[0]
+    input_combined = pd.DataFrame({'const': 1, 'W': [w], 'X': [x]})
+
+    y_pred = model.predict(input_combined)[0]
   
     # Log prediction
     with open("output.txt", "w") as f:
-      f.write(f"Input x: {x}\nPrediction: {y_pred}\n")
-
-    return jsonify({"x": x, "prediction": y_pred})
+      f.write(f"Input w:{w} x: {x}\nPrediction: {y_pred}\n")
+    return jsonify({"w": w, "x": x, "prediction": y_pred}), 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
